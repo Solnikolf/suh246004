ee507cdbc9e7c00d6861d54f2be6340db3b4d633
Суханов Андрей <asuhanovf@gmail.com>
2025-09-24 19:56:58 +0500

    final

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Владимир Маяковский");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Я");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("По мостовой");
+            Console.WriteLine("моей души изъезженной");
+            Console.WriteLine("шаги помешанных");
+            Console.WriteLine("вьют жестких фраз пяты.");
+            Console.WriteLine("Где города");
+            Console.WriteLine("повешены");
+            Console.WriteLine("и в петле о́блака");
+            Console.WriteLine("застыли");
+            Console.WriteLine("башен");
+            Console.WriteLine("кривые выи —");
+            Console.WriteLine("иду");
+            Console.WriteLine("один рыдать,");
+            Console.WriteLine("что перекрестком");
+            Console.WriteLine("ра́спяты");
+            Console.WriteLine("городовые.");
+
+            Console.ResetColor();
+
+        }
+    }
+}
