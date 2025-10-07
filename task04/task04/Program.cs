41e8c59997d25d185ab4056ae0078afdf73fe48f
Суханов Андрей <asuhanovf@gmail.com>
2025-10-07 20:34:48 +0500

    done

---
+++ task04/task04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число");
+
+            var x = double.Parse(Console.ReadLine());
+
+            var y = F(x);
+            Console.WriteLine("y = " + y);
+        }
+        static double F(double x) => Math.Abs(2 * Math.Sin(-3 * Math.Abs((x + 1) / 2)));
+    }
+}
