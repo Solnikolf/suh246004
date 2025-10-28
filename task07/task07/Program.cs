736cc6472bc13d1825b459b2d614716325dcf4f6
Admin <asuhanovf@gmail.com>
2025-10-28 11:35:44 +0500

    done

---
+++ task07/task07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число b:");
+            var b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число c:");
+            var c = int.Parse(Console.ReadLine());
+
+            if (SummOfaTriangle(a, b, c))
+                Console.WriteLine("Треугольник существует");
+            else Console.WriteLine("Такого треугольника не существует");
+        }
+
+        static bool SummOfaTriangle(int a, int b, int c) => (a + b > c) && (b + c > a) && (c + a > b);
+    }
+}
